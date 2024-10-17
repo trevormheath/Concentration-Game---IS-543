7d9103b146f497e16f47bbf64864dc7ce671a29c
trevormheath <trevormheath@gmail.com>
2024-10-17 08:05:25 -0600

    Initial Commit

---
+++ Concentration Game/Concentration_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration_GameApp.swift
+//  Concentration Game
+//
+//  Created by IS 543 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Concentration_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
